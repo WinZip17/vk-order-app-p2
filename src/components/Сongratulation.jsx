95d44545eb722b6969540405c2e0492289d30427
WinZip <wydo@protonmail.com>
2019-10-21 22:20:47 +0300

    Initial commit

---
+++ src/components/Сongratulation.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "../css/Сongratulation.css"
+
+
+const Сongratulation = (props) => {
+
+    return (
+        <div className="СongratulationFormSell">
+            <div className='Сongratulation mx-auto'>
+                <h2>Мы свяжемся с вами в ближайшее время</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Сongratulation;
